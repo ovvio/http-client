797d348582f79546d5e52e448340dbe6fccf27cc
Nazir Khusnutdinov <nazir@ovvio.pro>
2024-07-24 12:50:26 +0300

    Add `ResponseInterface`

---
+++ src/Response/ResponseInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ovvio\Component\HttpClient\Response;
+
+/**
+ * HTTP response.
+ */
+interface ResponseInterface
+{
+    /**
+     * Gets the HTTP status code of the response.
+     */
+    public function getStatusCode(): Enum\ResponseStatusCodeEnum;
+
+    /**
+     * Gets the HTTP headers of the response.
+     *
+     * @return string[][] The headers of the response keyed by header names in lowercas
+     */
+    public function getHeaders(): array;
+
+    /**
+     * Gets the response body as a string.
+     */
+    public function getContent(): string;
+
+    /**
+     * Gets the response body decoded as array, typically from a JSON payload.
+     *
+     * @return null|array<array-key, mixed>
+     */
+    public function toArray(): null|array;
+}
