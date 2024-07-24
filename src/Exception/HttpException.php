85c5d0bf98c64074da13787c69ad44a9c0171684
Nazir Khusnutdinov <nazir@ovvio.pro>
2024-07-24 19:19:50 +0300

    Add `statusCode` to `HttpException`

--- src/Exception/HttpException.php
+++ src/Exception/HttpException.php
@@ -4,8 +4,36 @@ declare(strict_types=1);
 
 namespace Ovvio\Component\HttpClient\Exception;
 
-use LogicException;
+use Ovvio\Component\HttpClient\Response\Enum\ResponseStatusCodeEnum;
+use Ovvio\Exceptions\BaseException;
+use Ovvio\Exceptions\Exceptions;
+use Throwable;
 
-class HttpException extends LogicException
+class HttpException extends BaseException
 {
+    /**
+     * @param ResponseStatusCodeEnum $statusCode HTTP response status codes
+     */
+    public function __construct(
+        private ResponseStatusCodeEnum $statusCode = ResponseStatusCodeEnum::HTTP_INTERNAL_SERVER_ERROR,
+        string $message = 'HTTP exception',
+        null|int $code = Exceptions::EXCEPTION_CODE_DEFAULT,
+        null|Throwable $previous = null,
+    ) {
+        parent::__construct(
+            message: $message,
+            code: $code ?? Exceptions::EXCEPTION_CODE_DEFAULT,
+            previous: $previous,
+        );
+    }
+
+    /**
+     * Get the value of statusCode
+     *
+     * @return ResponseStatusCodeEnum
+     */
+    public function getStatusCode(): ResponseStatusCodeEnum
+    {
+        return $this->statusCode;
+    }
 }
